2254fdcabd09ee8bf1217634a52a47cbf9eccf0b
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2021-10-30 19:47:27 +0300

    Code simplified

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 gulp.task("style", function(){
-    gulp.src("./scss/style.scss")
+    gulp.src("./scss/*.scss")
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest("./css"));
 });
