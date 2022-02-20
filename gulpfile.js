12331371dbd9cca2b9b881c1bd3dc7726cb58099
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-02-20 14:59:16 +0300

    Removed old project

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-
-gulp.task("sass", function(){
-    gulp.src("./scss/*.scss")
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest("./css"));
-});
